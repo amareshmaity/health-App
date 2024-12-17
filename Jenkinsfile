37840070f0371020477a9a9511dbbfaa99350b21
coderesh <amareshmaity2002@gmail.com>
2024-12-17 18:33:52 +0530

    modify for aws

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,12 @@
 pipeline {
     agent any
 
+    environment{
+        SSH_KEY_PATH='D:\\healthapp1.pem'
+        SSH_USER='ec2-user'
+        SSH_HOST='51.20.1.118'
+    }
+
     stages {
         stage('checkout') {
             steps {
@@ -31,6 +37,7 @@ pipeline {
         stage('Deploy') {
             steps {
                 bat """
+                ssh -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} ^ 
                 docker stop health-app || true
                 docker rm health-app || true
                 docker rmi amareshmaity/health-app:latest || true
--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Connect to Database
 // const DB_URL = process.env.MONGO_URL;
 // const DB_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/FatimaHealthAPPDB';
-const DB_URL = process.env.MONGO_URI || 'mongodb+srv://amareshmaity2002:X37tl4hLW7O8T86T@cluster0.g2ktfbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const DB_URL = process.env.MONGO_URI || 'mongodb+srv://amareshmaity2002:X37tl4hLW7O8T86T@cluster0.g2ktfbf.mongodb.net/HealthAPP?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(DB_URL)
   .then(() => {
